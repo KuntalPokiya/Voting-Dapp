e74fa9ebd0b5bf89f59e1f3c37e3bf8dbb6baa82
KuntalPokiya <137473512+KuntalPokiya@users.noreply.github.com>
2024-05-28 11:01:43 +0800

    Update VotingStatus.jsx
    added functionability for the front-end so that it can interact (send values) to smart contract

--- src/components/votingStatus/VotingStatus.jsx
+++ src/components/votingStatus/VotingStatus.jsx
@@ -2,8 +2,17 @@ import { useState, useEffect, useContext } from "react";
 import { WalletContext } from "../wallet/Wallet";
 
 const VotingStatus = () => {
-  const [voteStatus, setVoteStatus] = useState("");
-  const statusColor = true === "Voting in progress" ? "#2DFF2D" : "red";
+  const {contract} = useContext(WalletContext);
+  const [voteStatus, setVoteStatus] = useState(null);
+
+  useEffect(()=>{
+    const statusVoting=async()=>{
+      const status=await contract.methods.votingStatus().call();
+      setVoteStatus(status)
+    }
+    contact && statusVoting()
+  },[contract])
+  const statusColor = voteStatus === "Voting in progress" ? "#2DFF2D" : "red";
 
   return (
     <div style={{ display: "flex" }}>
